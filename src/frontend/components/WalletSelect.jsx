64eb34e41ad09998b3e6e89ba70530136668d552
Gaiseric <sebastiaancrisan@gmail.com>
2022-09-11 15:33:46 +0200

    minimum styling wallet select component

--- src/frontend/components/WalletSelect.jsx
+++ src/frontend/components/WalletSelect.jsx
@@ -6,36 +6,43 @@ import Button from '@mui/material/Button';
 import metamaskImg from "../../assets/mm.png";
 import walletconnectImg from "../../assets/wc.png";
 
+import "./WalletSelect.css";
+
 export const WalletSelect = (props) => {
 
   const handleClose = () => {
     props.setOpen(false);
   }
-  
+
   return (
     <Modal
         open={props.open}
         onClose={handleClose}
     >
-        <Box>
+        <Box className="walletSelect">
             <Button 
+                className="walletImgBtn"
                 variant="contained"
                 size="large"
                 onClick={() => {
                     activate(connectors.walletConnect);
                     setProvider("walletConnect");
                     handleClose();
-            }}
-        ><img src={walletconnectImg} alt="walletconnect"></img>Wallet Connect</Button>
+                }}
+            >
+                <img src={walletconnectImg} alt="walletconnect" className='walletImg'></img>
+                WalletConnect
+            </Button>
         <Button 
-                variant="contained"
-                size="large"
-                onClick={() => {
-                    activate(connectors.injected);
-                    setProvider("injected");
-                    handleClose();
+            className="walletImgBtn"
+            variant="contained"
+            size="large"
+            onClick={() => {
+                activate(connectors.injected);
+                setProvider("injected");
+                handleClose();
             }}
-        ><img src={metamaskImg} alt="metamask"></img>Metamask</Button>
+        ><img src={metamaskImg} alt="metamask" className='walletImg'></img>Metamask</Button>
         </Box>
     </Modal>
   )
